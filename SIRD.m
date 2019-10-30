442881eb138dab403e77063b5b792a47327324e9
WillWu88 <williamwu8-8@outlook.com>
2019-10-30 13:53:15 -0500

    Finished Multiple Cities Scenario, started working on graphing function

--- SIRD.m
+++ SIRD.m
@@ -1,4 +1,3 @@
-
 %SIR Model
 xt = [1; 0; 0; 0];
 updateMatrix = [0.95 0.04 0 0; 0.05 0.85 0 0; 0 0.1 1 0; 0 0.01 0 1];
@@ -6,24 +5,26 @@ max_iter = 500;
 time_series = 1:max_iter;
 
 % initializing population groups by city: STL, NYC and Beijing
-sSTL = zeros(max_iter, 1);
-iSTL = zeros(max_iter, 1);
-rSTL = zeros(max_iter, 1);
-dSTL = zeros(max_iter, 1);
-sNYC = zeros(max_iter, 1);
-iNYC = zeros(max_iter, 1);
-rNYC = zeros(max_iter, 1);
-dNYC = zeros(max_iter, 1);
-sBJC = zeros(max_iter, 1);
-iBJC = zeros(max_iter, 1);
-rBJC = zeros(max_iter, 1);
-dBJC = zeros(max_iter, 1);
+% as a n by 4 matrix, each column denotes a category such as infected
+
+% setting population as 1 for now, fill in for future reference
+STL = zeros(max_iter, 4);
+STLPop = 1;
+xtSTL = xt * STLPop;
+
+NYC = zeros(max_iter, 4);
+NYCPop = 1;
+xtNYC = xt * NYCPop;
+
+BJC = zeros(max_iter, 4);
+BJCPop = 1;
+xtBJC = xt * BJCPop;
 
 umSTL = updateMatrix;
 umNYC = [0.85 0.05 0 0; 0.15 0.8 0 0; 0 0.05 1 0; 0 0.1 0 1];
 umBJC = [0.6 0.1 0 0; 0.4 0.6 0 0; 0 0.1 1 0; 0 0.2 0 1];
 
-% below 
+% below
 sTravel_rate = [0 0.02 0.01; 0.03 0 0.05; 0.06 0.04 0];
 iTravel_rate = [0 0.01 0; 0.08 0 0; 0 0 0];
 rTravel_rate = [0 0.01 0.01; 0.01 0 0.01; 0.01 0.01 0];
@@ -34,53 +35,39 @@ Travel_rate = cat(3, sTravel_rate, iTravel_rate, rTravel_rate);
 genUMISO = genMultiUpdateISO(umSTL, umNYC, umBJC);
 genUMCON = genMultiUpdateCON(umSTL, umNYC, umBJC, Travel_rate);
 
-xt_temp = num2cell(xt);
-[sSTL(1), iSTL(1), rSTL(1), dSTL(1)] = deal(xt_temp{:});
 % run section ------------------------------------------------------------------
-for i = 2:max_iter
+for i = 1:max_iter
     % simulate single city: STL
-    xt = update_xt(updateMatrix, xt);
+    % xt = update_xt(updateMatrix, xt);
+    % xt_temp = num2cell(xt);
+    % [STL(i, 1), STL(i, 2), STL(i, 3), STL(i, 4)] = deal(xt_temp{:});
+
+    % simulate multiple city: STL, NYC and BJC
+    if (i==1)
+        % concatonating the 3 state vectors into a 12*1 state vector
+        xt = cat(1, xtSTL, xtNYC, xtBJC);
+    end
     xt_temp = num2cell(xt);
-    [sSTL(i), iSTL(i), rSTL(i), dSTL(i)] = deal(xt_temp{:});
+    [STL(i, 1), STL(i, 2), STL(i, 3), STL(i, 4)] = deal(xt_temp{1:4});
+    [NYC(i, 1), NYC(i, 2), NYC(i, 3), NYC(i, 4)] = deal(xt_temp{5:8});
+    [BJC(i, 1), BJC(i, 2), BJC(i, 3), BJC(i, 4)] = deal(xt_temp{9:12});
+
+    % isolated scenario
+    %xt = update_xt(genUMISO,xt);
+
+    % connected scenario
+    xt = update_xt(genUMCON, xt);
 end
+
+plotMultipleSIRD(STL, NYC, BJC, time_series, 'SIRD Plot of STL, NYC and BJC');
 
 figure('Name', 'SIRD Plot over 500 iterations')
 hold on;
-plot(time_series, sSTL)
-plot(time_series, iSTL)
-plot(time_series, rSTL)
-plot(time_series, dSTL)
+plot(time_series, STL(:, 1))
+plot(time_series, STL(:, 2))
+plot(time_series, STL(:, 3))
+plot(time_series, STL(:, 4))
 legend('Susceptible', 'Infected', 'Recovered', 'Dead');
-
-hold on;
-figure();
-subplot(3,1,1);
-hold on;
-plot(time_series, sSTL);
-plot(time_series, iSTL);
-plot(time_series, rSTL);
-plot(time_series, dSTL);
-title('SIRD Plot 1 over 500 iterations');
-legend('Susceptible','Infected','Recovered','Dead');
-
-subplot(3,1,2);
-hold on;
-plot(time_series, sNYC);
-plot(time_series, iNYC);
-plot(time_series, rNYC);
-plot(time_series, dNYC);
-title('SIRD Plot 2 over 500 iterations');
-legend('Susceptible','Infected','Recovered','Dead');
-
-subplot(3,1,3);
-hold on;
-plot(time_series, sBJC);
-plot(time_series, iBJC);
-plot(time_series, rBJC);
-plot(time_series, dBJC);
-title('SIRD Plot 3 over 500 iterations');
-legend('Susceptible','Infected','Recovered','Dead');
-hold off;
 
 function xtPlusOne = update_xt(updateMatrix, xt)
     % takes two arguments: an update matrix and a target matrix
@@ -102,24 +89,58 @@ function updateMatrix = genMultiUpdateCON(umCity1, umCity2, umCity3, trIncidence
     % assume trIncidence is 3*3*3, first layer for s, second for i, third for r
     isoUM = genMultiUpdateISO(umCity1, umCity2, umCity3);
     [r, c, l] = size(trIncidence);
-    for layerIndex = 1:l % cycling through three category
+    for layerIndex = 1:l% cycling through three category
         for rindex = 1:r
-           for cindex = 1:c
-            if rindex == cindex
-                index = 4*cindex+layerIndex-4;
-                entry = isoUM(index, index) - sum(trIncidence(rindex,:,layerIndex));
-                isoUM(index, index) = entry;
-            else
-                row = 4*rindex+layerIndex-4;
-                column = 4*cindex+layerIndex-4;
-                isoUM(row, column) = isoUM(row, column) + trIncidence(cindex, rindex, layerIndex);
-                % flipped cindex and rindex, since the loop is looping through 
-                % rows then columns, but the filling order should be columns
-                % then rows
-                
+            for cindex = 1:c
+                if rindex == cindex
+                    index = 4 * cindex + layerIndex - 4;
+                    entry = isoUM(index, index) - sum(trIncidence(rindex, :, layerIndex));
+                    isoUM(index, index) = entry;
+                else
+                    row = 4 * rindex + layerIndex - 4;
+                    column = 4 * cindex + layerIndex - 4;
+                    isoUM(row, column) = isoUM(row, column) + trIncidence(cindex, rindex, layerIndex);
+                    % flipped cindex and rindex, since the loop is looping through
+                    % rows then columns, but the filling order should be columns
+                    % then rows
+
+                end
             end
-           end
         end
     end
     updateMatrix = isoUM;
+end
+
+function plotMultipleSIRD(city1, city2, city3, time_series, plot_label)
+    figure();
+    title(plot_label);
+    subplot(3, 1, 1);
+    hold on;
+    plot(time_series, city1(:,1));
+    plot(time_series, city1(:,2));
+    plot(time_series, city1(:,3));
+    plot(time_series, city1(:,4));
+    title(city1);
+    legend('Susceptible', 'Infected', 'Recovered', 'Dead');
+    hold off;
+
+    subplot(3, 1, 2);
+    hold on;
+    plot(time_series, city2(:,1));
+    plot(time_series, city2(:,2));
+    plot(time_series, city2(:,3));
+    plot(time_series, city2(:,4));
+    title('SIRD Plot 2 over 500 iterations');
+    legend('Susceptible', 'Infected', 'Recovered', 'Dead');
+    hold off;
+
+    subplot(3, 1, 3);
+    hold on;
+    plot(time_series, city3(:,1));
+    plot(time_series, city3(:,2));
+    plot(time_series, city3(:,3));
+    plot(time_series, city3(:,4));
+    title('SIRD Plot 3 over 500 iterations');
+    legend('Susceptible', 'Infected', 'Recovered', 'Dead');
+    hold off;
 end
